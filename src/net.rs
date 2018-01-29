a9d19198385d93e78c005cd4a4ced22d235d6656
cahu <cahu@gmx.us>
2018-01-29 20:33:34 +0100

    num layer variable

--- src/net.rs
+++ src/net.rs
@@ -4,6 +4,8 @@
 use nalgebra::{DMatrix, DVector};
 
 pub struct Net {
+    /// number of layers
+    num_layers: usize,
     /// weighted inputs for each layer (before application of the sigmoid)
     z: Vec<DVector<f64>>,
     /// activations
@@ -24,18 +26,20 @@ fn sigmoid_prime(z: f64) -> f64 {
 
 impl Net {
     pub fn new(layers_sizes: &[usize]) -> Self {
-        assert!(layers_sizes.len() >= 2);
+        // number of layers
+        let num_layers = layers_sizes.len();
+        assert!(num_layers >= 2);
 
         // Activation value of neurons. aa[0] contains the input.
-        let mut aa = Vec::with_capacity(layers_sizes.len());
+        let mut aa = Vec::with_capacity(num_layers);
         aa.push(DVector::from_element(layers_sizes[0], 0f64));
 
         // Biases. bb[0] won't be used since layer 0 is the input layer.
-        let mut bb = Vec::with_capacity(layers_sizes.len());
+        let mut bb = Vec::with_capacity(num_layers);
         bb.push(DVector::from_element(0, 0f64));
 
         // Weigthed inputs. zz[0] won't be used since layer 0 corresponds to the input layer.
-        let mut zz = Vec::with_capacity(layers_sizes.len());
+        let mut zz = Vec::with_capacity(num_layers);
         zz.push(DVector::from_element(0, 0f64));
 
         for &sz in layers_sizes[1..].iter() {
@@ -48,7 +52,7 @@ impl Net {
         // is the weight between the k-th neuron of layer l-1 and the j-th neuron in layer l
         // (weigths[0] won't be used).  Indices j and k appear reversed but the order is intended
         // to let us use matrix multiplication.
-        let mut ww = Vec::with_capacity(layers_sizes.len());
+        let mut ww = Vec::with_capacity(num_layers);
         ww.push(DMatrix::from_element(0, 0, 0.0));
 
         for window in layers_sizes.windows(2) {
@@ -56,7 +60,9 @@ impl Net {
             let j = window[1]; // number of neurons in layer l
             ww.push(DMatrix::from_element(j, k, 0.5));
         }
+
         Net {
+            num_layers: num_layers,
             z: zz,
             a: aa,
             w: ww,
@@ -64,22 +70,26 @@ impl Net {
         }
     }
 
+    pub fn output(&self) -> &[f64] {
+        self.a[self.num_layers - 1].as_slice()
+    }
+
     pub fn feed(&mut self, input: &[f64]) -> &[f64] {
         // set values of the input layer
         self.a[0].as_mut_slice().copy_from_slice(input);
 
         // feed forward, layer 0 is the input layer so skip it
-        for l in 1 .. self.a.len() {
+        for l in 1 .. self.num_layers {
             self.compute_activation(l);
         }
 
         // return values of the output layer
-        self.a[self.a.len()-1].as_slice()
+        self.output()
     }
 
     fn compute_activation(&mut self, layer: usize) {
         assert!(layer > 0);
-        assert!(layer < self.a.len());
+        assert!(layer < self.num_layers);
         self.z[layer] = { // weighted input
             let activations = &self.a[layer-1];
             let weigths     = &self.w[layer];
